81958ed2ef0d010f6d5ad70b8680eb9f246a494f
Udon-Tobira <146440502+Udon-Tobira@users.noreply.github.com>
2024-08-26 16:28:15 +0900

    Add mac support to PreBuild

---
+++ PreBuild.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+set -e
+
+# Function to check if a command exists
+assureHasCommand() {
+    if ! command -v "$1" &> /dev/null; then
+        echo "$1 is not recognized as a command."
+        exit 9009
+    fi
+}
+
+# Function to execute a command and check its result
+assureExecute() {
+    "$@"
+    if [ $? -ne 0 ]; then
+        echo "Command \"$*\" failed."
+        exit $?
+    fi
+}
+
+# Main script
+(
+    # Change to the target directory
+    cd "$(dirname "$0")/Source/ThirdParty/assimp/assimp"
+
+    # Ensure cmake command exists
+    assureHasCommand cmake
+
+    # Run cmake commands
+    assureExecute cmake CMakeLists.txt
+    assureExecute cmake --build . --config Release
+)
